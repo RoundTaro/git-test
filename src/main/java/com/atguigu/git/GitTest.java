c246c26149516a5a599dd2de416b4f063e32e7af
wy <1604612449@qq.com>
2023-07-14 12:54:58 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wy
+ * @create 2023-07-14 12:51
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello git!");
+    }
+}
